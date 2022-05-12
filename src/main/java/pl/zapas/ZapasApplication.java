007b4fd787316b48a6d1f146657ae73d766993ef
noskowiak <97466059+noskowiak@users.noreply.github.com>
2022-05-12 19:34:30 +0200

    Initial commit

---
+++ src/main/java/pl/zapas/ZapasApplication.java
@@ -0,0 +1,13 @@
+package pl.zapas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZapasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZapasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/zapas/ZapasApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zapas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZapasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
